a5c235b6b1646567b2e468b4d8f07d9acec18bc5
Awet Tsegazeab <avvvet@gmail.com>
2024-03-12 01:26:24 -0400

    NATS connection

--- main.go
+++ main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"log"
 	"os"
+
+	"github.com/nats-io/nats.go"
 )
 
 func main() {
 
 	url := os.Getenv("NATS_URL")
-	log.Println(url)
+	if url == "" {
+		url = "nats://localhost:4222"
+	}
+
+	nc, err := nats.Connect(url)
+	if err != nil {
+		log.Printf("Error: unbale to connect to NATS server %v", err)
+	}
+
+	log.Printf("Connected to NATS server at %s", url)
+
+	// Close the NATS connection when done
+	defer nc.Close()
 }
